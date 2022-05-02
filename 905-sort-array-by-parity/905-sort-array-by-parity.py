46cfc59ec570f513fe3c151f8759a667b0da2b7e
Bharath Ithal <98535307+bharathithal@users.noreply.github.com>
2022-05-02 11:10:22 +0530

    Time: 79 ms (90.70%), Space: 14.7 MB (63.70%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.py
+++ 905-sort-array-by-parity/905-sort-array-by-parity.py
@@ -1,12 +1,11 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        res = []
-        for num in nums:
-            if num % 2 == 0:
-                res.append(num)
-
-        for num in nums:
-            if num % 2 != 0:
-                res.append(num)
-
-        return res
+        beg, end = 0, len(nums) - 1
+        
+        while beg <= end:
+            if nums[beg] % 2 == 0:
+                beg += 1
+            else:
+                nums[beg], nums[end] = nums[end], nums[beg]
+                end -= 1
+        return nums
